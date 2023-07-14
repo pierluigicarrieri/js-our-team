730a660454e40e6996a8f6761fd0e5328412032a
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-14 18:20:52 +0200

    created array of objects

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,42 @@
 "use strict"
+
+const teamMembers = [
+    {
+        firstName : "Wayne",
+        lastName : "Barnett",
+        position : "Founder & CEO",
+        profilePic : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        firstName : "Angela",
+        lastName : "Caroll",
+        position : "Chief Editor",
+        profilePic : "angela-caroll-chief-editor.jpg"
+    },
+    {
+        firstName : "Walter",
+        lastName : "Gordon",
+        position : "Office Manager",
+        profilePic : "walter-gordon-office-manager.jpg"
+    },
+    {
+        firstName : "Angela",
+        lastName : "Lopez",
+        position : "Social Media Manager",
+        profilePic : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        firstName : "Scott",
+        lastName : "Estrada",
+        position : "Developer",
+        profilePic : "scott-estrada-developer.jpg"
+    },
+    {
+        firstName : "Barbara",
+        lastName : "Ramos",
+        position : "Graphic Designer",
+        profilePic : "barbara-ramos-graphic-designer.jpg"
+    },
+];
+
+console.log(teamMembers)
